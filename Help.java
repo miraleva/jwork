e47b68a892b189796e1eac8748c3354bb5b2a195
Almira <miramirza74@gmail.com>
2024-07-19 23:56:01 +0300

    help

--- Help.java
+++ Help.java
@@ -1,3 +1,11 @@
 import java.util.Scanner;
 
-public class Input
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Adınızı girin: ");
+        String name = scanner.nextLine();
+        System.out.println("Merhaba, " + name + "!");
+        scanner.close();
+    }
+}
