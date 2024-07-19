e86443f4c9525c6aac1ff3275c022867318acd1b
Almira <miramirza74@gmail.com>
2024-07-19 23:45:51 +0300

    ya

---
+++ .vscode/Math.java
@@ -0,0 +1,8 @@
+public class Math {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Toplam: " + sum);
+    }
+}
