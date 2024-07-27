b7c37240135f4292f6e1049659ef472b775b68aa
almira mirza <almira@mirza.com>
2024-07-27 17:42:52 +0300

    ok

---
+++ people_source/humanB.java
@@ -0,0 +1,18 @@
+package people_source;
+
+public class humanB {
+    private String name;
+    private int age;
+     
+    public humanB(String ad, int age){
+        this.name=ad;
+        this.age= age;
+     }
+public String getName() {
+    return this.name;
+}
+public int getAge(){
+    return this.age;
+}   
+
+}
