9aae7abcbc42f46002522de9794032381f090457
almira mirza <almira@mirza.com>
2024-08-17 14:54:26 +0300

    a

--- LinkListo/LinkoListo.java
+++ LinkListo/LinkoListo.java
@@ -1,15 +1,30 @@
-public class LinkoListo  {
-    Node startNode; 
+public class LinkoListo {
+    Node startNode;
 
-    public LinkoListo(){
-        this.startNode= null;
+    public LinkoListo() {
+        this.startNode = null;
     }
 
-    public void append(int data){
+    public void append(int data) {
         Node newNode = new Node(data);
-        if(this.startNode ==null){
-           this.startNode= newNode;
-           return;
+        if (this.startNode == null) {
+            this.startNode = newNode;
+            return;
         }
+
+        Node lastNode = this.startNode;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
+    public void printList() {
+        Node currentNode = this.startNode;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println();
     }
 }
