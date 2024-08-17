69346e98b09e806340be9059dc30b860731d442e
almira mirza <almira@mirza.com>
2024-08-17 14:56:41 +0300

    mainn

---
+++ LinkListo/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        LinkoListo myList= new LinkoListo();
+        myList.append(1);
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+
+        myList.printList(); 
+        
+    }
+}
