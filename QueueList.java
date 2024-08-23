018d66d700d2aac3b942d2a8c285a00fd38b5fef
almira mirza <almira@mirza.com>
2024-08-23 16:24:20 +0300

    queue

--- QueueList.java
+++ QueueList.java
@@ -1,21 +1,61 @@
-
-
 public class QueueList {
 
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+
+        QueueNode result = queue.dequeue();
+        System.out.println(result.val);
+        result = queue.dequeue();
+        System.out.println(result.val);
+
+        queue.enqueue(17);
+        result = queue.dequeue();
+        System.out.println(result.val);
+    }
+
 }
 
+class Queue {
+    QueueNode firstNode = null; 
+    QueueNode lastNode = null; 
 
+   
+    public void enqueue(int val) {
+        QueueNode temp = new QueueNode(val); 
 
-class LinkList {
+        if (this.firstNode == null) { 
+            this.firstNode = temp;
+            this.lastNode = temp; 
+        } else {
+            this.lastNode.nextNode = temp; 
+            this.lastNode = temp;
+        }
+    }
+
     
+    public QueueNode dequeue() {
+        if (this.firstNode == null) {
+            return null; 
+        }
+        QueueNode temp = this.firstNode; 
+        this.firstNode = this.firstNode.nextNode; 
+        if (this.firstNode == null) {
+            this.lastNode = null; 
+        }
+        return temp; 
+    }
 }
 
+class QueueNode {
+    int val; 
+    QueueNode nextNode; 
 
-class Node{
-    int val;
-    Node nextNode;
-
-    public Node(int val){
-        this.val=val;
+    public QueueNode(int val) {
+        this.val = val; 
+        this.nextNode = null; 
     }
 }
