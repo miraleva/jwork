9cc7773ed5afdb99ba8fc064f7e48674ce8a74cf
almira mirza <almira@mirza.com>
2024-08-23 16:27:01 +0300

    quee

--- QueueList.java
+++ QueueList.java
@@ -7,32 +7,30 @@ public class QueueList {
         queue.enqueue(3);
         queue.enqueue(4);
 
-        QueueNode result = queue.dequeue();
+        QueueNode result = queue.dequeue(); 
         System.out.println(result.val);
-        result = queue.dequeue();
+        result = queue.dequeue(); 
         System.out.println(result.val);
 
         queue.enqueue(17);
-        result = queue.dequeue();
+        result = queue.dequeue(); 
         System.out.println(result.val);
     }
-
 }
 
 class Queue {
-    QueueNode firstNode = null; 
-    QueueNode lastNode = null; 
+    QueueNode firstNode = null;
+    QueueNode lastNode = null;
 
-   
+    
     public void enqueue(int val) {
-        QueueNode temp = new QueueNode(val); 
-
-        if (this.firstNode == null) { 
-            this.firstNode = temp;
-            this.lastNode = temp; 
+        QueueNode newNode = new QueueNode(val);
+        if (this.firstNode == null) {
+            this.firstNode = newNode;
+            this.lastNode = newNode;
         } else {
-            this.lastNode.nextNode = temp; 
-            this.lastNode = temp;
+            this.lastNode.nextNode = newNode;
+            this.lastNode = newNode;
         }
     }
 
@@ -41,21 +39,21 @@ class Queue {
         if (this.firstNode == null) {
             return null; 
         }
-        QueueNode temp = this.firstNode; 
-        this.firstNode = this.firstNode.nextNode; 
+        QueueNode temp = this.firstNode;
+        this.firstNode = this.firstNode.nextNode;
         if (this.firstNode == null) {
             this.lastNode = null; 
         }
-        return temp; 
+        return temp;
     }
 }
 
 class QueueNode {
     int val; 
-    QueueNode nextNode; 
+    QueueNode nextNode;
 
     public QueueNode(int val) {
-        this.val = val; 
-        this.nextNode = null; 
+        this.val = val;
+        this.nextNode = null;
     }
 }
