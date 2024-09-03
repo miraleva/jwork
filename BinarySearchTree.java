8ba3b35bb8701ff10131def9fdfb71482fd92a75
almira mirza <almira@mirza.com>
2024-09-03 03:26:05 +0300

    bst

---
+++ BinarySearchTree.java
@@ -0,0 +1,25 @@
+ //node class lzm +
+ //little sola big sağa
+ //min ve max değerini yazdıran method 
+
+ class Node{
+    int value;
+    Node left = null;
+    Node right = null;
+
+    Node(int value){
+        this.value = value;
+    }
+ }
+
+
+ 
+public class BinarySearchTree{
+    Node root =null;
+    
+    public void addValue(int value){
+        if (root=null;)
+    }
+
+}
+
