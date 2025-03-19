172478e86d03c9bebb7fa451a20bdb06c0eb806f
almira mirza <miramirza74@gmail.com>
2025-03-19 23:53:39 +0300

    a

--- StudentGrades/Student.java
+++ StudentGrades/Student.java
@@ -1,32 +1,25 @@
 package StudentGrades;
 //burası tm gibi sadece constructorda notlar derslere özgü onu tekleştirsem mi bilemedim 
 
-public class Student{
+public class Student {
     String name;
     int age;
-    int mathNote;
-    int historyNote;
-    int deathNote;
-
-   
-    public Student(String name, int age,int mathNote, int historyNote, int deathNote){ //3 ders olcak + 5 öğrenci 
-        this.name=name;
-        this.age=age;
-        this.mathNote=mathNote;
-        this.historyNote=historyNote;
-        this.deathNote=deathNote;
+    int ID;
+    public Student(String name, int age, int ID) { // 3 ders olcak + 5 öğrenci
+        this.name = name;
+        this.age = age;
+        this.ID= ID;
 
     }
 
-    public void student_info(){
-        System.out.println("Student: "+ this.name + " Age: "+ this.age + " Math: "+ this.mathNote +" History: "+ this.historyNote +" Death: "+ this.deathNote);
+    public void student_info() {
+        System.out.println("Student: " + this.name + " Age: " + this.age + " Math: " + this.mathNote + " History: "
+                + this.historyNote + " Death: " + this.deathNote);
 
     }
 
-    public int averajStudent(){
-        return (mathNote + historyNote + deathNote)/ 3;
+    public int averajStudent() {
+        return (mathNote + historyNote + deathNote) / 3;
     }
-
-    
 
 }
