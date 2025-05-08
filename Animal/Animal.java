6be559326c790f78aa5f9d8354b067f866f47c24
almira mirza <miramirza74@gmail.com>
2025-05-08 23:40:40 +0300

    animall

---
+++ Animal/Animal.java
@@ -0,0 +1,43 @@
+package Animal;
+
+public class Animal{
+
+    String name;
+    
+    Animal(String Name){
+        this.name=name;
+
+    }
+
+    public void makeSound(){
+        System.out.println(name + "makes sound be aware...");
+    }
+    
+    class Dog extends Animal {
+        Dog(String name){
+            super(name);
+        }
+        @Override
+        public void makeSound(){
+            System.out.println(name + "Barks");
+        }
+
+
+    }
+
+    class Cat extends Animal {
+        Cat(String name){
+            super(name);
+        }
+        @Override
+        public void makeSound(){
+            System.out.println(name + "meows");
+        }
+
+
+    }
+
+
+}
+
+
