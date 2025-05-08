ec055a20e4f9601e50afa86ebcfd1e10270f6a42
almira mirza <miramirza74@gmail.com>
2025-05-08 23:45:41 +0300

    main animal

---
+++ Animal/Main.java
@@ -0,0 +1,16 @@
+package Animal;
+
+public class ZooTest {
+    public static void main(String args[]){
+        Animal animal = new Animal("penguen");
+        Dog animal1 = new Dog("kangal");
+        Cat animal2 = new Cat("kedi");
+
+         animal.makeSound();
+         animal1.makeSound();
+         animal2.makeSound();
+   
+
+    }
+    
+}
